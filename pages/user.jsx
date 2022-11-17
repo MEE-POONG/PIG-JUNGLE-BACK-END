e70f38ecad9ee0a32faf498010fe2662e3965b67
mind2421 <thutiya.ch@gmail.com>
2022-11-17 16:35:55 +0700

    UserAndAddress-mind

---
+++ pages/user.jsx
@@ -0,0 +1,191 @@
+import Head from 'next/head';
+import { useState } from 'react';
+import IndexPage from "components/layouts/IndexPage";
+import { useRouter } from 'next/router';
+import { Container, Table, Button, Form, OverlayTrigger, Badge,Modal } from 'react-bootstrap';
+import { FaTimes, FaEye, FaEdit, FaHandHoldingUsd, FaReply } from 'react-icons/fa';
+
+export default function TransferPage() {
+
+  const [createModal, setCreateModal] = useState(false);
+  const [deleteModal, setDeleteModal] = useState(false);
+  const [editModal, setEditModal] = useState(false);
+
+  const createClose = () => setCreateModal(false);
+  const createShow = () => setCreateModal(true);
+
+  const editClose = () => setEditModal(false);
+  const editShow = () => setEditModal(true);
+
+  const deleteClose = () => setDeleteModal(false);
+  const deleteShow = () => setDeleteModal(true);
+
+  return (
+    < >
+      <Head>
+        <title>HOME | dxx=</title>
+        <meta
+          name="description"
+          content="I2AROBOT 2"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Container fluid className=" pt-4 px-4">
+        <div className="bg-secondary rounded p-4">
+          <div className="d-flex align-items-center justify-content-between mb-4">
+            <Button variant="primary" className='btn-square'>
+              <FaReply />
+            </Button>
+            <h5 className="mb-0 w-m-max me-2">ชื่อผู้ใช้</h5>
+            <Button variant="primary" className='ms-2 w-m-max' onClick={createShow}>
+              เพิ่มสมาชิก
+            </Button>
+          </div>
+
+          <div className="d-flex align-items-center border-bottom py-2">
+            <div className="table-responsive w-100">
+              <Table className="table table-striped align-middle  align-items-center ">
+                <thead>
+                  <tr>
+                    <th >id</th>
+                    <th >รูปผู้ใช้</th>
+                    <th >ชื่อผู้ใช้</th>
+                    <th >email</th>
+                    <th >เบอร์โทรศัพท์</th>
+                    <th >วันที่สมัคร</th>
+                    <th >จัดการ</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <th>1</th>
+                    <td><img className="img-fluid rounded-circle mx-auto mb-4" src="images/testimonial-1.jpg" style={{ width: "40px", height: "40px"  }} /></td>
+                    <td>นายมา แล้วไปไหนไม่รู้</td>
+                    <td>gmail@hotmail.com</td>
+                    <td>08888888</td>
+                    <td>31/12/2023 </td>
+                    <td>
+                      <Button className="btn btn-sm btn-success m-2" onClick={editShow}>แก้ไข</Button>
+                      <Button className="btn btn-sm btn-danger m-2" onClick={deleteShow}>ลบสมาชิก</Button>
+                    </td>
+                  </tr>
+                  <tr>
+                    <th>1</th>
+                    <td><img className="img-fluid rounded-circle mx-auto mb-4" src="images/testimonial-1.jpg" style={{ width: "40px", height: "40px"  }} /></td>
+                    <td>นายมา แล้วไปไหนไม่รู้</td>
+                    <td>gmail@hotmail.com</td>
+                    <td>08888888</td>
+                    <td>31/12/2023 </td>
+                    <td>
+                      <Button className="btn btn-sm btn-success m-2" onClick={editShow}>แก้ไข</Button>
+                      <Button className="btn btn-sm btn-danger m-2" onClick={deleteShow}>ลบสมาชิก</Button>
+                    </td>
+                  </tr>
+                  <tr>
+                    <th>1</th>
+                    <td><img className="img-fluid rounded-circle mx-auto mb-4" src="images/testimonial-1.jpg" style={{ width: "40px", height: "40px"  }} /></td>
+                    <td>นายมา แล้วไปไหนไม่รู้</td>
+                    <td>gmail@hotmail.com</td>
+                    <td>08888888</td>
+                    <td>31/12/2023 </td>
+                    <td>
+                      <Button className="btn btn-sm btn-success m-2" onClick={editShow}>แก้ไข</Button>
+                      <Button className="btn btn-sm btn-danger m-2" onClick={deleteShow}>ลบสมาชิก</Button>
+                    </td>
+                  </tr>
+                </tbody>
+              </Table>
+            </div>
+          </div>
+        </div>
+      </Container>
+
+      <Modal show={createModal} onHide={createClose} centered className="bg-templant">
+        <Modal.Header closeButton >
+          <Modal.Title>เพิ่มสมาชิก</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+        <Form.Group controlId="formFile" className="mb-3">
+            <Form.Label>ชื่อผู้ใช้</Form.Label>
+            <Form.Control type="text" defaultValue='' />
+          </Form.Group>
+          <Form.Group controlId="formFile" className="mb-3">
+            <Form.Label>email</Form.Label>
+            <Form.Control type="text" defaultValue='' />
+          </Form.Group>
+          <Form.Group controlId="formFile" className="mb-3">
+            <Form.Label>เบอร์โทรศัพท์</Form.Label>
+            <Form.Control type="text" defaultValue='' />
+          </Form.Group>
+          <Form.Group controlId="formFile" className="mb-3">
+            <Form.Label>รหัสผ่าน</Form.Label>
+            <Form.Control type="password" />
+          <Form.Group controlId="formFile" className="mb-3">
+            <Form.Label>รูปผู้ใช้</Form.Label>
+            <Form.Control type="file" />
+          </Form.Group>
+          </Form.Group>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={createClose}>
+            ยกเลิก
+          </Button>
+          <Button variant="primary" onClick={createClose}>
+            สร้าง
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
+      <Modal show={editModal} onHide={editClose} centered className="bg-templant">
+        <Modal.Header closeButton >
+          <Modal.Title>แก้ไขสมาชิก</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form.Group controlId="formFile" className="mb-3">
+            <Form.Label>ชื่อผู้ใช้</Form.Label>
+            <Form.Control type="text" defaultValue='นายมา แล้วไปไหนไม่รู้' />
+          </Form.Group>
+          <Form.Group controlId="formFile" className="mb-3">
+            <Form.Label>email</Form.Label>
+            <Form.Control type="text" defaultValue='gmail@hotmail.com' />
+          </Form.Group>
+          <Form.Group controlId="formFile" className="mb-3">
+            <Form.Label>เบอร์โทรศัพท์</Form.Label>
+            <Form.Control type="text" defaultValue='08888888888' />
+          </Form.Group>
+          <Form.Group controlId="formFile" className="mb-3">
+            <Form.Label>รูปผู้ใช้</Form.Label>
+            <Form.Control type="file" />
+          </Form.Group>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={editClose}>
+            ยกเลิก
+          </Button>
+          <Button variant="primary" onClick={editClose}>
+            ยืนยัน
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
+      <Modal show={deleteModal} onHide={deleteClose} centered className="bg-templant">
+        <Modal.Header closeButton >
+          <Modal.Title>ต้องการลบผู้ใช้ .......</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form.Label>ยืนยันการลบผู้ใช้</Form.Label>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={deleteClose}>
+            ยกเลิก
+          </Button>
+          <Button variant="primary" onClick={deleteClose}>
+            ยืนยัน
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
+    </ >
+  );
+}
+TransferPage.layout = IndexPage;
