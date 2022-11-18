36b6c7ff152f14ef967b7b90f51af9fcb6ed1857
Pattawut Biakrathok <62004847+bens0746@users.noreply.github.com>
2022-11-18 11:42:09 +0700

    promotion

--- pages/promotions/index.jsx
+++ pages/promotions/index.jsx
@@ -4,13 +4,22 @@ import { Container, Modal, Button, Form, Image, InputGroup, Row, Col } from 'rea
 import { FaEdit, FaPlus, FaTrash } from 'react-icons/fa'
 
 export default function PromotionPage() {
+
+    // modal
+    const [showModalCreate, setShowModalCreate] = useState(false);
+    const [showModalEdit, setShowModalEdit] = useState(false);
+
+    const ShowModalCreate = () => setShowModalCreate(true);
+    const ShowModalEdit = () => setShowModalEdit(true);
+    const CloseModal = () => { setShowModalCreate(false), setShowModalEdit(false)};
+
     return (
         <>
             <Container fluid className="pt-4 px-4">
                 <div className="bg-secondary text-center rounded shadow p-4">
                     <div className="d-flex align-items-center justify-content-between mb-4">
                         <h6 className="mb-0">รายการเข้าร่วม</h6>
-                        <Button variant="success">
+                        <Button variant="success" onClick={ShowModalCreate}>
                             <FaPlus />
                         </Button>
                     </div>
@@ -20,8 +29,8 @@ export default function PromotionPage() {
                                 <tr className="text-white">
                                     <th scope="col">รูปสินค้า</th>
                                     <th scope="col">ชื่อสินค้า</th>
+                                    <th scope="col">ชื่อโปรโมชั่น</th>
                                     <th scope="col">โปรโมชั่น</th>
-                                    <th scope="col">รายละเอียด</th>
                                     <th scope="col">ราคาเดิม</th>
                                     <th scope="col">ราคาโปรโมชั่น</th>
                                     <th scope="col">จัดการ</th>
@@ -38,7 +47,7 @@ export default function PromotionPage() {
                                     <td className='text-decoration-line-through'>2000 บาท</td>
                                     <td className='text-danger'>1000 บาท</td>
                                     <td>
-                                        <a className="btn btn-sm btn-success me-2"><FaEdit /></a>
+                                        <a className="btn btn-sm btn-success me-2" onClick={ShowModalEdit}><FaEdit /></a>
                                         <a className="btn btn-sm btn-danger me-2" href=""><FaTrash /></a>
                                     </td>
                                 </tr>
@@ -52,7 +61,7 @@ export default function PromotionPage() {
                                     <td className='text-decoration-line-through'>50 บาท</td>
                                     <td className='text-danger'>20 บาท</td>
                                     <td>
-                                        <a className="btn btn-sm btn-success me-2"><FaEdit /></a>
+                                        <a className="btn btn-sm btn-success me-2" onClick={ShowModalEdit}><FaEdit /></a>
                                         <a className="btn btn-sm btn-danger me-2" href=""><FaTrash /></a>
                                     </td>
                                 </tr>
@@ -66,7 +75,7 @@ export default function PromotionPage() {
                                     <td className='text-decoration-line-through'>5000 บาท</td>
                                     <td className='text-danger'>2000 บาท</td>
                                     <td>
-                                        <a className="btn btn-sm btn-success me-2"><FaEdit /></a>
+                                        <a className="btn btn-sm btn-success me-2" onClick={ShowModalEdit}><FaEdit /></a>
                                         <a className="btn btn-sm btn-danger me-2" href=""><FaTrash /></a>
                                     </td>
                                 </tr>
@@ -75,6 +84,97 @@ export default function PromotionPage() {
                     </div>
                 </div>
             </Container>
+
+            {/* Create */}
+            <Modal show={showModalCreate} onHide={CloseModal} centered className="bg-templant">
+                <Modal.Header closeButton >
+                    <Modal.Title>เพิ่มโปรโมชั่น</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form.Group controlId="formFile" className="mb-3">
+                        <Form.Label className='d-block'>รูปสินค้า</Form.Label>
+                    </Form.Group>
+                    <Form.Group controlId="formFile" className="mb-3">
+                        <Form.Label>ชื่อสินค้า</Form.Label>
+                        <Form.Select>
+                            <option>--- เลือกสินค้า ---</option>
+                            <option value={1}>บ้อง</option>
+                            <option value={2}>บ้องขวดอิชิตัน</option>
+                            <option value={3}>กัญชา</option>
+                        </Form.Select>
+                    </Form.Group>
+                    <Form.Group controlId="formFile" className="mb-3">
+                        <Form.Label className='d-block'>ชื่อโปรโมชั่น</Form.Label>
+                        <Form.Control type="text" defaultValue={""} />
+                    </Form.Group>
+                    <Form.Group controlId="formFile" className="mb-3">
+                        <Form.Label>รายละเอียดโปรโมชั่น</Form.Label>
+                        <Form.Control as="textarea" rows={3} defaultValue={""} />
+                    </Form.Group>
+                    <Form.Group controlId="formFile" className="mb-3">
+                        <Form.Label className='d-block'>สูตรคำนวน</Form.Label>
+                        <Form.Control type="text" defaultValue={""} />
+                    </Form.Group>
+                    <Form.Group controlId="formFile" className="mb-3">
+                        <Form.Label>ราคา</Form.Label>
+                        <InputGroup className="mb-3">
+                            <Form.Control type="number" defaultValue={""} />
+                            <InputGroup.Text>บาท</InputGroup.Text>
+                        </InputGroup>
+                    </Form.Group>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={CloseModal}>
+                        ยกเลิก
+                    </Button>
+                    <Button variant="success">
+                        เพิ่ม
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+
+             {/* Edit */}
+             <Modal show={showModalEdit} onHide={CloseModal} centered className="bg-templant">
+                <Modal.Header closeButton >
+                    <Modal.Title>แก้ไขโปรโมชั่น</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form.Group controlId="formFile" className="mb-3">
+                        <Form.Label className='d-block'>รูปสินค้า</Form.Label>
+                    </Form.Group>
+                    <Form.Group controlId="formFile" className="mb-3">
+                        <Form.Label>ชื่อสินค้า</Form.Label>
+                        <Form.Control type="text" defaultValue={"บ้องแก้ว"} disabled/>
+                    </Form.Group>
+                    <Form.Group controlId="formFile" className="mb-3">
+                        <Form.Label className='d-block'>ชื่อโปรโมชั่น</Form.Label>
+                        <Form.Control type="text" defaultValue={"9.9"} />
+                    </Form.Group>
+                    <Form.Group controlId="formFile" className="mb-3">
+                        <Form.Label>รายละเอียดโปรโมชั่น</Form.Label>
+                        <Form.Control as="textarea" rows={3} defaultValue={"ซื้อ 5 แถม 1"} />
+                    </Form.Group>
+                    <Form.Group controlId="formFile" className="mb-3">
+                        <Form.Label className='d-block'>สูตรคำนวน</Form.Label>
+                        <Form.Control type="text" defaultValue={"x*(0.5)"} />
+                    </Form.Group>
+                    <Form.Group controlId="formFile" className="mb-3">
+                        <Form.Label>ราคา</Form.Label>
+                        <InputGroup className="mb-3">
+                            <Form.Control type="number" defaultValue={"2000"} />
+                            <InputGroup.Text>บาท</InputGroup.Text>
+                        </InputGroup>
+                    </Form.Group>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={CloseModal}>
+                        ยกเลิก
+                    </Button>
+                    <Button variant="success">
+                        เพิ่ม
+                    </Button>
+                </Modal.Footer>
+            </Modal>
         </>
     )
 }
